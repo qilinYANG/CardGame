b7d8e59acc09868fd4b1ddd6894effd73adb27a2
Chenwei Cui <frankcuish@gmail.com>
2021-10-14 13:53:36 -0400

    Fixed: compare and optimalPoint

--- src/PointGame.java
+++ src/PointGame.java
@@ -31,7 +31,7 @@ public class PointGame extends Game {
     public void PlayerAction(PokerPlayer ppl) {
         out:while(true){
             System.out.println("Please select your action: 1. hit  2. split 3. double up  4. stand");
-            int index = Utils.safeIntInput("Please input your selection (1 to 4): ",1,4);
+            int index = Utils.safeIntInput("Please input your selection (1 to 4): ", 1, 4);
             switch (index){
                 case 1:
                     ppl.addCard(deck.pop());
@@ -63,28 +63,53 @@ public class PointGame extends Game {
      */
 
     public void optimalPoint(PokerPlayer ppl) {
-        int sum = 0,sumWthAc = 0,AcNum = 0,sumtmp = 0;
+        int sum = 0;
+        boolean hasAce = false;
+        ArrayList<PokerCard> hand = ppl.getHand();
 
-        for (PokerCard pc : ppl.getHand()) {
-            sum += Double.parseDouble(pc.getValue());
-            if (pc.getValue().equals("1")) AcNum+=1;
-        }
-        if(sum > points) {
-            ppl.setScore(-1);}
-        else {
-            sumWthAc = sum;
+        for (PokerCard pc : hand) {
+            String value = pc.getValue();
+            if (value.equals("1")) hasAce = true;
 
-            for (int i = 0; i < AcNum; i++) {
-                sumtmp = sumWthAc;
-                sumWthAc += 10;
-                if (sumWthAc > points) break;
+            if (value.equals("jack") ||
+                value.equals("queen") ||
+                value.equals("king")){
+                    sum += 10;
             }
-            int sumOpt = sumWthAc > points? sumtmp:sumWthAc;
-            if(sumOpt == points && AcNum == 1)
-                ppl.setScore(points+0.5);
-            else
-                ppl.setScore(sumOpt);
+            else {
+                sum += Double.parseDouble(value);
+            }
         }
+
+        // If bust
+        if (sum > points) {
+            ppl.setScore(-1);
+            return;
+        }
+
+        // note: "If the hand consists of more than one Ace, only one Ace can count as 1."
+
+        // If already optimal
+        if ((!hasAce) || (sum + 10 > points)) {  // 10 = -1 + 11 (Ace can be 1 or 11)
+            ppl.setScore(sum);
+            return;
+        }
+
+        sum += 10;  // Make it optimal
+
+        // If deserves a bonus
+        // note: A natural Blackjack is defined as the starting hand having a value of 21 (i.e. an Ace and any face card).
+        // note: A natural Trianta Ena is defined as a hand having a value of 31 (i.e. an Ace and two face cards).
+        // note: A face card is a playing card that is a king, queen, or jack of a suit.
+        boolean condition_1 = sum == points;
+        boolean condition_2 = (points - 1) / 10 == (float) hand.size();
+        if (condition_1 && condition_2) {
+            ppl.setScore(sum + 0.5);
+            return;
+        }
+        
+        ppl.setScore(sum);
+        return;
     }
 
     /**
@@ -124,9 +149,16 @@ public class PointGame extends Game {
      * @param dealer
      * @return
      */
-    public String compare(PokerPlayer player, PokerPlayer dealer) {
+    public String compare(PokerPlayer player, PokerPlayer dealer, boolean dealerAdvantage) {
+        // note: (31) "In the case of a tie, the Dealer wins."
+        // note: (31) "A natural 31 of the Banker results in the Banker winning the bets from all players."
+        // note: (21) "In the case of a tie, the Player bet is returned and a new round starts."
+        // 21: no dealerAdvantage; 31: dealerAdvantage
         if (player.getScore() > dealer.getScore()) return "Player";
         if (dealer.getScore() > player.getScore()) return "Dealer";
+        if (dealerAdvantage) {
+            return "Dealer";
+        }
         return "Draw";
 
     }
